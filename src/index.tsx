df56398bb6a3394a44be73304ef412229cd5d522
Jack <40455730+idogmat@users.noreply.github.com>
2022-12-19 15:25:29 +0300

    hashR

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,14 @@ import * as serviceWorker from './serviceWorker';
 import {Provider} from "react-redux";
 import store from "./store/store";
 import App from "./App";
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
 root.render(
     <Provider store={store}>
-        <BrowserRouter>
-    <App/>
-        </BrowserRouter>
+        <HashRouter>
+            <App/>
+        </HashRouter>
     </Provider>);
 
 // If you want your app to work offline and load faster, you can change
